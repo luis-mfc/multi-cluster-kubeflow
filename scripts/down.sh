e39a1033be893a1ba124f07f07c87d53b359cefe
Luis Costa <luis.mfc@protonmail.com>
2024-05-10 18:19:42 +0100

    switch to kind

--- scripts/down.sh
+++ scripts/down.sh
@@ -5,14 +5,11 @@ set -u
 source "$(dirname $0)/../.env"
 
 delete_cluster() {
-  local profile=$1
+  local cluster=$1
 
-  minikube delete \
-    -p "$profile"
+  kind delete cluster --name "$cluster"
 }
 
 for cluster in "${CLUSTERS[@]}"; do
   delete_cluster "$cluster" || true
 done
-
-docker network rm "$NETWORK_NAME" || true
--- scripts/up.sh
+++ scripts/up.sh
@@ -6,13 +6,13 @@ set -eu -o pipefail
 source "$(dirname $0)/../.env"
 
 install_dependencies() {
-  local profile=$1
+  local $cluster=$1
 
   helm repo add jetstack https://charts.jetstack.io
   helm repo update >/dev/null
 
   helm upgrade --install cert-manager jetstack/cert-manager \
-    --kube-context "$profile" \
+    --kube-context "kind-$cluster" \
     --namespace cert-manager \
     --create-namespace \
     --version v1.13.1 \
@@ -21,7 +21,7 @@ install_dependencies() {
     --wait
 
   helm upgrade --install admiralty oci://public.ecr.aws/admiralty/admiralty \
-    --kube-context "$profile" \
+    --kube-context "kind-$cluster" \
     --namespace admiralty \
     --create-namespace \
     --version 0.16.0 \
@@ -30,19 +30,13 @@ install_dependencies() {
 }
 
 create_cluster() {
-  local profile=$1
+  local cluster=$1
   local kubernetes_version=$2
-  local network=$3
-  local static_ip=$4
 
-  set -x
-  minikube status -p "$profile" >/dev/null 2>&1 || minikube start \
-    -p "$profile" \
-    --kubernetes-version "$kubernetes_version" \
-    --network "$network" \
-    --static-ip "$static_ip"
-  set +x
-  install_dependencies "$profile"
+  kind create cluster \
+    --name "$cluster" \
+    --image "kindest/node:$kubernetes_version"
+  install_dependencies "$cluster"
 }
 
 cross_cluster_authentication() {
@@ -51,24 +45,24 @@ cross_cluster_authentication() {
 
   kubectl label nodes \
     --all "topology.kubernetes.io/region=$workload_cluster" \
-    --context "$workload_cluster"
+    --context "kind-$workload_cluster"
 
   kubectl \
-    --context "$workload_cluster" \
+    --context "kind-$workload_cluster" \
     create serviceaccount "$management_cluster" || true
 
-  TOKEN=$(kubectl --context "$workload_cluster" create token "$management_cluster")
+  TOKEN=$(kubectl --context "kind-$workload_cluster" create token "$management_cluster")
   IP=$(kubectl config view | yq ".clusters[] | select(.name == \"$workload_cluster\") | .cluster.server" |
     awk -F/ '{print $3}' | awk -F: '{print $1}')
-  CONFIG=$(kubectl --context "$workload_cluster" config view --minify=true --raw --output json |
-    jq '.users[0].user={token:"'$TOKEN'"} | .clusters[0].cluster.server="https://'$IP':6443"')
+  CONFIG="$(kubectl --context "kind-$workload_cluster" config view --minify=true --raw --output json |
+    jq '.users[0].user={token:"'$TOKEN'"} | .clusters[0].cluster.server="https://'$IP':6443"')"
 
   echo "TOKEN=$TOKEN"
   echo "IP=$IP"
   echo "CONFIG=$CONFIG"
 
   kubectl \
-    --context "$management_cluster" \
+    --context "kind-$management_cluster" \
     create secret generic "$workload_cluster" \
     --from-literal=config="$CONFIG"
 }
@@ -78,7 +72,7 @@ multi_cluster_scheduling() {
   workload_cluster=$2
 
   # Create a Target for each workload cluster:
-  cat <<EOF | kubectl --context "$management_cluster" apply -f -
+  cat <<EOF | kubectl --context "kind-$management_cluster" apply -f -
 apiVersion: multicluster.admiralty.io/v1alpha1
 kind: Target
 metadata:
@@ -89,7 +83,7 @@ spec:
 EOF
 
   # In the workload cluster, create a Source for the management cluster:
-  cat <<EOF | kubectl --context "$workload_cluster" apply -f -
+  cat <<EOF | kubectl --context "kind-$workload_cluster" apply -f -
 apiVersion: multicluster.admiralty.io/v1alpha1
 kind: Source
 metadata:
@@ -99,18 +93,9 @@ spec:
 EOF
 }
 
-docker network create \
-  --driver=bridge \
-  --subnet=192.168.49.0/24 \
-  --gateway=192.168.49.1 -o \
-  --ip-masq -o \
-  --icc -o com.docker.network.driver.mtu=65535 \
-  --label=created_by.minikube.sigs.k8s.io=true \
-  --label=name.minikube.sigs.k8s.io=minikube "$NETWORK_NAME"
-
 echo "Creating clusters..."
-create_cluster "${CLUSTERS[0]}" "$KUBERNETES_VERSION" "$NETWORK_NAME" "192.168.49.2"
-create_cluster "${CLUSTERS[1]}" "$KUBERNETES_VERSION" "$NETWORK_NAME" "192.168.49.3"
+create_cluster "${CLUSTERS[0]}" "$KUBERNETES_VERSION"
+create_cluster "${CLUSTERS[1]}" "$KUBERNETES_VERSION"
 
 echo "Cross cluster auth setup..."
 cross_cluster_authentication "${CLUSTERS[0]}" "${CLUSTERS[1]}"
