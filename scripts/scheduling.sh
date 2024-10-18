1f7cfdb801c9cdd070045d81ef9227c8ea651b0d
Luis Costa <luis.mfc@protonmail.com>
2024-10-18 20:46:06 +0100

    simplify admiralty script further

--- scripts/scheduling.sh
+++ scripts/scheduling.sh
@@ -10,72 +10,57 @@ set -eu -o pipefail
 source "$(dirname "$0")/../.env"
 
 cross_cluster_authentication() {
-  cluster=$1
-  target_cluster=$2
-  target_cluster_name="$(kubectl --context "$target_cluster" config view --minify -o jsonpath='{.clusters[].name}')"
-  namespace=$3
-
   kubectl \
-    --context "$target_cluster" \
-    create serviceaccount "$cluster" \
-    -n "$namespace" || true
+    --context "$CLOUD_CLUSTER_CONTEXT" \
+    create serviceaccount "$DC_CLUSTER_NAME" \
+    -n "admiralty" || true
 
-  TOKEN=$(kubectl --context "$target_cluster" create token "$cluster" -n "$namespace")
-  CONFIG="$(kubectl --context "$target_cluster" config view --minify=true --raw --output json |
+  TOKEN=$(kubectl --context "$CLOUD_CLUSTER_CONTEXT" create token "$DC_CLUSTER_NAME" -n "admiralty")
+  CONFIG="$(kubectl --context "$CLOUD_CLUSTER_CONTEXT" config view --minify=true --raw --output json |
     jq '.users[0].user={token:"'"$TOKEN"'"}')"
 
   kubectl \
-    --context "$target_cluster" \
-    create secret generic "$target_cluster" \
+    --context "$DC_CLUSTER_CONTEXT" \
+    create secret generic "$CLOUD_CLUSTER_NAME" \
     --from-literal=config="$CONFIG" \
-    -n "$namespace" || true
+    -n "admiralty" || true
 }
 
 multi_cluster_scheduling() {
-  cluster=$1
-  cluster_name="$(kubectl --context "$cluster" config view --minify -o jsonpath='{.clusters[].name}')"
-  target_cluster=$2
-  target_cluster_name="$(kubectl --context "$target_cluster" config view --minify -o jsonpath='{.clusters[].name}')"
-  namespace=$3
-
-  cat <<EOF | kubectl --context "$cluster" apply -f -
+  cat <<EOF | kubectl --context "$DC_CLUSTER_CONTEXT" apply -f -
 apiVersion: multicluster.admiralty.io/v1alpha1
 kind: ClusterTarget
 metadata:
-  name: $target_cluster_name
+  name: $CLOUD_CLUSTER_NAME
 spec:
   kubeconfigSecret:
-    name: $target_cluster_name
-    namespace: $namespace
+    name: $CLOUD_CLUSTER_NAME
+    namespace: admiralty
 EOF
 
-  cat <<EOF | kubectl --context "$target_cluster" apply -f -
+  cat <<EOF | kubectl --context "$CLOUD_CLUSTER_CONTEXT" apply -f -
 apiVersion: multicluster.admiralty.io/v1alpha1
 kind: ClusterSource
 metadata:
-  name: $cluster_name
+  name: $DC_CLUSTER_NAME
 spec:
   serviceAccount:
-    name: $cluster
-    namespace: $namespace
+    name: $DC_CLUSTER_NAME
+    namespace: admiralty
 EOF
 }
 
 self_cluster_scheduling() {
-  cluster=$1
-  cluster_name="$(kubectl --context "$cluster" config view --minify -o jsonpath='{.clusters[].name}')"
-
-  # Create a Target for the self cluster:
-  cat <<EOF | kubectl --context "$cluster" apply -f -
+  cat <<EOF | kubectl --context "$DC_CLUSTER_CONTEXT" apply -f -
 apiVersion: multicluster.admiralty.io/v1alpha1
 kind: ClusterTarget
 metadata:
-  name: $cluster_name
+  name: $DC_CLUSTER_NAME
 spec:
   self: true
 EOF
 }
 
-cross_cluster_authentication "$DC_CLUSTER_CONTEXT" "$CLOUD_CLUSTER_CONTEXT" default
-multi_cluster_scheduling "$DC_CLUSTER_CONTEXT" "$CLOUD_CLUSTER_CONTEXT" default
-self_cluster_scheduling "$DC_CLUSTER_CONTEXT"
+cross_cluster_authentication
+multi_cluster_scheduling
+self_cluster_scheduling
